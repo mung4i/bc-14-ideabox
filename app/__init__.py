54e83eeca5f3b1f2419ccd2c563c3d8f276d5be1
mungaiandela <jailbre3k@gmail.com>
2017-01-26 21:54:50 +0300

    init.py changed to accomodate hosting

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ login_manager = LoginManager()
 
 
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     Bootstrap(app)

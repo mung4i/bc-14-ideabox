564713723f4b1099009219005387b055698f9632
mungaiandela <jailbre3k@gmail.com>
2017-01-27 03:36:20 +0300

    Atomic commits

--- app/home/views.py
+++ app/home/views.py
@@ -17,7 +17,15 @@ def list_ideas():
     check_user()
     ideas = Data.query.filter_by(users_email=current_user.email).all()
         # return redirect(url_for('home.list_ideas'))
-    return render_template('home/index.html', ideas=ideas, title="List of ideas")
+    #functionality added to load comments
+    form=IdeaboxComments()
+    if form.validate_on_submit():
+        comment = Comments(comment=form.comment.data, users_email=current_user.email)
+        db.session.add(comment)
+        db.session.commit()
+
+    comments= Comments.query.filter_by(comment_id=id).all()
+    return render_template('home/index.html', ideas=ideas, form=form, comments=comments, title="List of ideas")
 
 
 @home.route('/ideabox/new', methods=['POST', 'GET'])
@@ -61,3 +69,18 @@ def edit_ideabox():
         flash('Edit successful')
         return redirect(url_for('home.list_ideas'))
     return render_template('home/ideabox.html', action="Edit", add_data=add_data, form=form, title="Edit idea", placeholder=form_title)
+
+@home.route('/ideabox/comments',methods=['POST','GET'])
+@login_required
+def comment_ideabox():
+    #function not working gives a 200 but does not post to db
+    form = IdeaboxComments()
+    if form.validate_on_submit():
+        comment = Comments(comment=form.comment.data, users_email=current_user.email)
+        db.session.add(comment)
+        db.session.commit()
+        flash('comment has been published')
+        return redirect(url_for('home.comment_ideabox'))
+    else:
+        flash('Comment publish failed', error)
+    return render_template('home/ideabox.html', action="Comment", query=query, form=form, title="Comment")
